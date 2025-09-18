2bc42fcf3f06bb9f9a275f81ddd3d45c74ad9c83
Vinícius Santos <vini.santos.valentim@gmail.com>
2025-09-18 16:51:25 -0300

    Create exerc1.c

---
+++ LP1/Atividade7/Structs/exerc1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+struct Personagem {
+        int forca;
+        int energia;
+        int experiencia;
+};
+
+int main (void)
+{
+        //iniciando personagem 1
+        struct Personagem p1;
+        p2.forca = 10;
+        p2.energia = 50;
+        p2.experiencia = 30;
+
+        //iniciando personagem 2
+        struct Personagem p2 = {18, 20, 25};
+
+        return 0;
+}
